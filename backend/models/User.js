06104bae6753816b63b9145672fc0ee636156af6
Abdul Haq <abdulhaq.swe@gmail.com>
2026-06-27 16:39:09 +0500

    Reduce bcrypt to 8 rounds

--- backend/models/User.js
+++ backend/models/User.js
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema(
 
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 8);
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
